274b8f4b121329d6a04cdfb3d1b006a1af4c4951
Matthew Pagan <mastapegs01@gmail.com>
2022-01-24 19:36:31 +0000

    working build

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 
 function load_js() {
     wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/js/test.js', array(), 1.0, true);
-    wp_enqueue_script( 'custom-element', get_template_directory_uri() . '/assets/js/custom-element.js', array(), 1.0, true);
+    wp_enqueue_script( 'custom-element', get_template_directory_uri() . '/dist/custom-element.js', array(), 1.0, true);
 }
 add_action("wp_enqueue_scripts", "load_js");
